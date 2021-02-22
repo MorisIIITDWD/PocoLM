760b661d61bced15b964d7dd88d9f60ae4bdb4c0
MorisIIITDWD <17bec012@iiitdwd.ac.in>
2021-02-22 17:51:27 +0530

    Post Porcessing script for OOV's.

--- postProcessing.py
+++ postProcessing.py
@@ -5,6 +5,7 @@
 # Import necessary libraries
 import os
 import sys
+import re
 import numpy as np 
 # Give the text file paths as an input.
 directory = sys.argv[1] # Where the training data is present.
@@ -13,7 +14,16 @@ devDir = sys.argv[3] # Where the 10% extracted lines are present.
 
 for file in os.listdir(directory):
     if file != "dev.txt":
-        None
+        lines = open(listDir + '/' + "unique_words_" + file + '.list','r').readlines()
+        with open(directory + '/' + file,'a') as source:
+            with open(devDir + + '/' + 'dev_' + file,'r') as dev:
+                text = source.read()
+                data = dev.readlines()
+                for words in lines:
+                    if ~re.search(words,text) :
+                        for line in data:
+                            if re.search(words,line):
+                                source.write(line)
 
 
 
