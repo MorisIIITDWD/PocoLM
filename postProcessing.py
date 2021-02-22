dc723820db01d67c90c0e9a5887759d16115cb04
MorisIIITDWD <17bec012@iiitdwd.ac.in>
2021-02-22 17:35:15 +0530

    Script to post proces the train set to remove OOV's.

---
+++ postProcessing.py
@@ -0,0 +1,28 @@
+# Program : To make the train set such that there are no OOV's.
+# Language : python3
+# Author : S.Moris
+
+# Import necessary libraries
+import os
+import sys
+import numpy as np 
+# Give the text file paths as an input.
+directory = sys.argv[1] # Where the training data is present.
+listDir = sys.argv[2] # Where the unique_word list is present.
+devDir = sys.argv[3] # Where the 10% extracted lines are present.
+
+for file in os.listdir(directory):
+    if file != "dev.txt":
+        
+
+
+
+
+
+
+
+
+
+
+
+
